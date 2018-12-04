d80ddc63e1ee2908568efc1fd8170bb2473709db
Lee-Heisenbug <1070068427@qq.com>
2018-12-05 01:02:37 +0800

    Add dat.gui to the lil_engine example.

--- lil_engine/index.js
+++ lil_engine/index.js
@@ -20,8 +20,36 @@ scene = new Object3D();
 scene.add( cubes[0] );
 scene.add( cubes[1] );
 
+camera.position.z = 5;
+camera.far = 50;
+
 function render(){
     renderer.renderScene(scene,camera);
 }
 
 render();
+
+var gui = new dat.GUI();
+
+cubes.forEach( ( cube, index ) => {
+    var cubeFolder = gui.addFolder('cube'+index);
+
+    ['position', 'rotation', 'scale'].forEach( property => {
+        var proFolder = cubeFolder.addFolder( property );
+
+        ['x', 'y', 'z'].forEach( axis => {
+            proFolder.add( cube[property], axis, -2, 2 ).onChange(render);
+        } )
+
+    } )
+} );
+
+var cameraFolder = gui.addFolder('camera');
+['fov', 'aspect', 'near', 'far'].forEach( property => {
+
+    cameraFolder.add( camera, property ).onChange( () => {
+        camera.updateProjectionMatrix();
+        render();
+    } );
+
+} )
